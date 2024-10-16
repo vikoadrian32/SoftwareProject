828debfb7ba0374f7fdb5b1c46222a72444031ae
Ralf <rafataalfatih55@gmail.com>
2024-10-16 10:01:57 +0700

    create html structure in view index and loginAdmin

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,3 +1,59 @@
-<div>
-    <!-- Always remember that you are absolutely unique. Just like everyone else. - Margaret Mead -->
+@extends('layouts.admin')
+@section('content')
+<div class="container mt-5">
+    <h1>Welcome Admin</h1>
 </div>
+  <div class="container" id="menu">
+    <div class="row ">
+            @foreach ($films as $film)
+            <div class="col-md-4 posterImg">
+              @foreach ($film->attributes as $item)
+              <img src="/photos/{{$item['poster']}}" class="rounded-4" >
+              @endforeach
+                <div class="info">
+                    <p style="font-size: 25px;" >{{$film['title']}}</p>
+                    <p style="font-weight: 400; font-size: 15px;">Director : {{$film['director']}}</p>
+                    <p style="font-weight: 400; font-size: 15px;">Duration : {{$film['duration']}}</p>
+                    <p style="font-weight: 400; font-size: 15px;">Release Date : {{$film['release_date']}}</p>
+                    <p style="font-weight: 400; font-size: 15px;">Genre :
+                      @foreach ($film->genres as $genre)
+                       {{ $genre->genre_name }}
+                       @if (!$loop->last)
+                         ,
+                       @endif
+                     @endforeach
+                      </p>
+
+                      <p style="font-weight: 400; font-size: 15px;">Stok : 
+                            @if (empty($film->stok->jumlah))
+                            <span style="color: red">Unavailable</span> 
+                            @else
+                                {{$film->stok->jumlah}}
+                            @endif
+                        </p>
+                    
+                      {{-- <p style="font-weight: 400; font-size: 15px;">Stok : 
+                        @foreach ($film->stok as $item)
+                            
+                            @if ($item->jumlah != 0)
+                            {{$item->jumlah}}
+                            @else
+                                <span style="color: red">Unavailable</span> 
+                            @endif
+                         
+                        @endforeach
+                        </p> --}}
+                    <button type="submit" class="btn btn-primary">
+                      <a href="/admin/update/{{$film->id}}" class="nav-link">Edit</a></button>
+                    <button type="submit" class="btn btn-danger">
+                      <a href="/admin/delete/{{$film->id}}" class="nav-link">Delete</a></button>
+                </div>
+                
+            </div>
+            @endforeach
+
+    </div>
+  </div>
+    
+@endsection
+
--- resources/views/admin/loginAdmin.blade.php
+++ resources/views/admin/loginAdmin.blade.php
@@ -1,3 +1,42 @@
-<div>
-    <!-- It is never too late to be what you might have been. - George Eliot -->
-</div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="{{asset('css/login.css')}}">
+</head>
+<body>
+    <div class="wrapper">
+
+        <div class="card-switch">
+          <label class="switch">
+            {{-- <input type="checkbox" class="toggle"> --}}
+            {{-- <span class="slider"></span> --}}
+            {{-- <span class="card-side"></span> --}}
+            <div class="flip-card__inner">
+              <div class="flip-card__front">
+                <div class="title">Log in</div>
+                <form class="flip-card__form" action="/admin/login"method="post">
+                  @csrf
+                  <input class="flip-card__input"  placeholder="Email" type="email" name="email">
+                  <input class="flip-card__input" placeholder="Password" type="password" name="password">
+                  <button class="flip-card__btn">Let`s go!</button>
+                </form>
+                @if(session('errors'))
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach(session('errors') as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+              @endif
+              </div>
+            </div>
+          </label>
+        </div>
+        
+      </div>
+</body>
+</html>
